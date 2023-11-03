0a771d270ee1cefd5417c279ff94b96866844794
Jigar Patel <jpatel@Jigars-MacBook-Pro.local>
2023-11-03 19:50:43 -0400

    Add ai declarations to all project files

---
+++ ms1/Date.h
@@ -0,0 +1,10 @@
+/*****************************************************************************
+Date.h
+
+Full Name               : Jigar Patel
+Student ID#             : 118005172
+Email                   : jdpatel22@myseneca.ca
+Date of completion      : 31 October 2023
+
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+******************************************************************************/
---
+++ ms1/Status.cpp
@@ -0,0 +1,10 @@
+/*****************************************************************************
+Status.cpp
+
+Full Name               : Jigar Patel
+Student ID#             : 118005172
+Email                   : jdpatel22@myseneca.ca
+Date of completion      : 1 November 2023
+
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+******************************************************************************/
