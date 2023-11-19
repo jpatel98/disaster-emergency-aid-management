77cfb8b046e13992eaa2886c1d0e04609bb7c4ea
Jigar <jpxtel98@outlook.com>
2023-11-19 13:20:35 -0500

    Update utils file

--- ms3/Utils.cpp
+++ ms3/Utils.cpp
@@ -9,23 +9,6 @@ Date of completion      : 1 November 2023
 I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 ******************************************************************************/
 
-/* Citation and Sources...
-Final Project Milestone 3
-Module: Date
-Filename: Utils.cpp
-Version 1.0
-Author	Ching an shih
-Revision History
------------------------------------------------------------
-I have done all the coding by myself and only copied the code
-that my professor provided to complete my workshops and assignments.
------------------------------------------------------------
-OR
------------------------------------------------------------
-Write exactly which part of the code is given to you as help and
-who gave it to you, or from what source you acquired it.
------------------------------------------------------------*/
-
 #include <iostream>
 #include <ctime>
 #include <cstring>
@@ -170,7 +153,6 @@ namespace sdds
 
    void Utils::extractChar(istream &istr, char ch) const
    {
-
       if (istr.peek() == ch)
       {
          istr.ignore();
